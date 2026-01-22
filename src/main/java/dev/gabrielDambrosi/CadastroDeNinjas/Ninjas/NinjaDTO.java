74c2c4cb31716bcdddfa3146e92997668c5e9c94
Gabriel_DAmbrosi <gabriel.dambrosipereira@gmail.com>
2026-01-22 10:49:13 -0300

    chore: inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x DTO

--- src/main/java/dev/gabrielDambrosi/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/gabrielDambrosi/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.gabrielDambrosi.CadastroDeNinjas.Ninjas;
 
+import dev.gabrielDambrosi.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private Integer idade;
+    private String imgUrl;
+    private MissoesModel missoes;
+    private String rank;
 }
